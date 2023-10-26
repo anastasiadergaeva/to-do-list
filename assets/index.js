40bf06f5b307693024ed2ab482d2dfff1b217e42
anastasiadergaeva <adergaeva@gmail.com>
2023-10-26 11:59:48 +0300

    add check to empty string

--- assets/index.js
+++ assets/index.js
@@ -4,14 +4,16 @@ const taskList = document.querySelector('.tasklist');
 const noTasks = document.querySelector('.notasks');
 
 function createTask() {
-    noTasks.classList.add('nodisplay');
     const taskValue = taskInput.value;
-    const newTask = document.createElement('li');
-    newTask.classList.add('pointer');
-    newTask.classList.add('font');
-    newTask.textContent = taskValue;
-    taskList.append(newTask);
-    taskInput.value = '';
+    if (taskValue !== '') {
+        noTasks.classList.add('nodisplay');
+        const newTask = document.createElement('li');
+        newTask.classList.add('pointer');
+        newTask.classList.add('font');
+        newTask.textContent = taskValue;
+        taskList.append(newTask);
+        taskInput.value = '';
+    }
 }
 
 function checkTask(newTask) {
