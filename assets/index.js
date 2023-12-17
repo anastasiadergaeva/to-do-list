bb0feea3f5fc1779ad87559964bdf818020d61df
anastasiadergaeva <adergaeva@gmail.com>
2023-12-17 20:15:11 +0300

    add some changes

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 const taskInput = document.querySelector('.taskinput');
-const addButton = document.querySelector('.button');
+const addButton = document.querySelector('.add-button');
+const clearButton = document.querySelector('.clear-button');
 const taskList = document.querySelector('.tasklist');
 const noTasks = document.querySelector('.notasks');
 
@@ -8,19 +9,52 @@ function createTask() {
     if (taskValue !== '') {
         noTasks.classList.add('nodisplay');
         const newTask = document.createElement('li');
-        newTask.classList.add('pointer');
         newTask.classList.add('font');
-        newTask.textContent = taskValue;
-        taskList.append(newTask);
+        newTask.classList.add('taskstyle');
+        taskList.appendChild(newTask);
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.classList.add('pointer');
+        checkbox.value = taskValue;
+        newTask.appendChild(checkbox);
+
+        const label = document.createElement('label');
+        label.textContent = taskValue;
+        newTask.appendChild(label);
         taskInput.value = '';
+    }
+    saveToLocalStorage();
+}
+
+function checkTask(checkbox) {
+    if (checkbox.target.tagName === 'LI') {
+        checkbox.target.classList.toggle('checked');
+        saveToLocalStorage();
     }
 }
 
-function checkTask(newTask) {
-    if (newTask.target.tagName === 'LI') {
-        newTask.target.classList.toggle('checked');
+function clearTaskList() {
+    taskList.innerHTML = '';
+    noTasks.classList.remove('nodisplay');
+    clearButton.disabled = true;
+    localStorage.clear();
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('tasklist'.taskList.innerHTML);
+}
+
+function showTaskList() {
+    taskList.innerHTML = localStorage.getItem('tasklist');
+    if (taskList.innerHTML !== '') {
+        clearButton.disabled = false;
+        noTasks.classList.add('nodisplay');
     }
 }
+
+showTaskList();
 
 addButton.addEventListener('click', createTask);
 taskList.addEventListener('click', checkTask);
+clearButton.addEventListener('click', clearTaskList);
